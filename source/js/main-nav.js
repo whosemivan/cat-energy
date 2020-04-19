7eecd13183bd7ba795c6114c8002bc39ef7d97ad
Ivan <vsem2004@gmail.com>
2020-04-19 14:38:36 +0300

    Адаптивные декоративные элементы

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,16 +1,8 @@
 var navMain = document.querySelector('.main-nav');
 var button = document.querySelector('.page-header__nav-button');
 
-
-
 button.addEventListener('click', function() {
-  if (button.classList.contains('page-header__nav-button--close')) {
-    navMain.classList.remove('main-nav--hide');
-    button.classList.remove('page-header__nav-button--close');
-    button.classList.add('page-header__nav-button--opened');
-  } else if (button.classList.contains('page-header__nav-button--opened')) {
-    navMain.classList.add('main-nav--hide');
-    button.classList.remove('page-header__nav-button--opened');
-    button.classList.add('page-header__nav-button--close');
-  }
+  navMain.classList.toggle('main-nav--block');
+  button.classList.toggle('page-header__nav-button--close');
+  button.classList.toggle('page-header__nav-button--opened');
 });
