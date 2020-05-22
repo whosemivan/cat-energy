8739ab73c4701ff2fb441f71f15506131fe2196e
Ivan <vsem2004@gmail.com>
2020-05-22 22:59:43 +0300

    Правки

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,7 +1,7 @@
 var navMain = document.querySelector('.main-nav');
 var button = document.querySelector('.page-header__nav-button');
 
-
+button.classList.remove('visually-hidden');
 navMain.classList.add('main-nav--hidden');
 button.addEventListener('click', function() {
   navMain.classList.toggle('main-nav--hidden');
